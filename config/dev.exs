9abe3edcece74998456c2ef3f1316b56db7fdaa2
probase <kanjachileshe@yahoo.com>
2023-05-12 04:43:04 -0400

    ui

--- config/dev.exs
+++ config/dev.exs
@@ -9,7 +9,7 @@ import Config
 config :agro, AgroWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4100],
+  http: [port: 4100],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
